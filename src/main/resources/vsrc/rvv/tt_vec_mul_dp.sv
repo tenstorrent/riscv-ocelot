2db3b775083ddeca43ed8f71907e7a6c074e3df3
Dongjie Xie <dxie@tenstorrent.com>
2023-05-11 20:54:20 -0500

    Recoded tt_vec_mul_dp to work around a verilator compilation error

--- src/main/resources/vsrc/rvv/tt_vec_mul_dp.sv
+++ src/main/resources/vsrc/rvv/tt_vec_mul_dp.sv
@@ -15,17 +15,12 @@ module tt_vec_mul_dp #(parameter
    
    always_ff @(posedge i_clk) begin
       if (i_mulen_0a) begin
-         for(int i=VLEN/16;i<VLEN/8; i++) o_sum_1a[i][ 16:0] <= {{ 9{i_issgn_0a && i_sized_src1_0a[i][ 7]}}, i_sized_src1_0a[i][ 7:0]} * {{ 9{i_issgnsrc2_0a && i_sized_src2_0a[i][ 7]}}, i_sized_src2_0a[i][ 7:0]};
-         for(int i=VLEN/32;i<VLEN/16;i++) o_sum_1a[i][ 32:0] <= {{17{i_issgn_0a && i_sized_src1_0a[i][15]}}, i_sized_src1_0a[i][15:0]} * {{17{i_issgnsrc2_0a && i_sized_src2_0a[i][15]}}, i_sized_src2_0a[i][15:0]};
-         for(int i=VLEN/64;i<VLEN/32;i++) o_sum_1a[i][ 64:0] <= {{33{i_issgn_0a && i_sized_src1_0a[i][31]}}, i_sized_src1_0a[i][31:0]} * {{33{i_issgnsrc2_0a && i_sized_src2_0a[i][31]}}, i_sized_src2_0a[i][31:0]};
-         for(int i=0;      i<VLEN/64;i++) o_sum_1a[i][128:0] <= {{65{i_issgn_0a && i_sized_src1_0a[i][63]}}, i_sized_src1_0a[i][63:0]} * {{65{i_issgnsrc2_0a && i_sized_src2_0a[i][63]}}, i_sized_src2_0a[i][63:0]};
+         for(int i=VLEN/16;i<VLEN/8; i++) o_sum_1a[i] <= {{112'b0, {9{i_issgn_0a && i_sized_src1_0a[i][7]}},  i_sized_src1_0a[i][7:0]}  * {{9 {i_issgnsrc2_0a && i_sized_src2_0a[i][7]}},  i_sized_src2_0a[i][7:0]}};
+         for(int i=VLEN/32;i<VLEN/16;i++) o_sum_1a[i] <= {{96'b0, {17{i_issgn_0a && i_sized_src1_0a[i][15]}}, i_sized_src1_0a[i][15:0]} * {{17{i_issgnsrc2_0a && i_sized_src2_0a[i][15]}}, i_sized_src2_0a[i][15:0]}};
+         for(int i=VLEN/64;i<VLEN/32;i++) o_sum_1a[i] <= {{64'b0, {33{i_issgn_0a && i_sized_src1_0a[i][31]}}, i_sized_src1_0a[i][31:0]} * {{33{i_issgnsrc2_0a && i_sized_src2_0a[i][31]}}, i_sized_src2_0a[i][31:0]}};
+         for(int i=0;      i<VLEN/64;i++) o_sum_1a[i] <= {{       {65{i_issgn_0a && i_sized_src1_0a[i][63]}}, i_sized_src1_0a[i][63:0]} * {{65{i_issgnsrc2_0a && i_sized_src2_0a[i][63]}}, i_sized_src2_0a[i][63:0]}};
       end
    end
    
-   always_comb begin
-      for(int i=VLEN/16;i<VLEN/8; i++) o_sum_1a[i][128:17] = '0;
-      for(int i=VLEN/32;i<VLEN/16;i++) o_sum_1a[i][128:33] = '0;
-      for(int i=VLEN/64;i<VLEN/32;i++) o_sum_1a[i][128:65] = '0;
-   end
-   
 endmodule
+
