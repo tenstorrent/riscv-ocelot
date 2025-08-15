04af0a59c872e171c8f04c447f9f389c33132b43
kychen <kychen@tenstorrent.com>
2025-08-15 02:19:49 -0500

    update autogen functions

--- src/main/resources/vsrc/vpu/autogen_riscv_imabfv.v
+++ src/main/resources/vsrc/vpu/autogen_riscv_imabfv.v
@@ -7982,22 +7982,22 @@ output reg [0:0] v_idivop
 
 always_comb begin
 	case({Opcode[6], Opcode[5], Opcode[4], Opcode[3], Opcode[2], Opcode[1], Opcode[0], funct7[6], funct7[5], funct7[4], funct7[3], funct7[2], funct7[1], funct3[2], funct3[1], funct3[0]})
-	    // vdiv.vv  - funct7=100001, funct3=000 (vm bit ignored)
-	    16'b1010111100001000  :  v_idivop[0] = 1'b1;
-	    // vdivu.vv - funct7=100000, funct3=000 (vm bit ignored)
-	    16'b1010111100000000  :  v_idivop[0] = 1'b1;
-	    // vrem.vv  - funct7=100011, funct3=000 (vm bit ignored)
-	    16'b1010111100011000  :  v_idivop[0] = 1'b1;
-	    // vremu.vv - funct7=100010, funct3=000 (vm bit ignored)
-	    16'b1010111100010000  :  v_idivop[0] = 1'b1;
-	    // vdiv.vx  - funct7=100001, funct3=100 (vm bit ignored)
-	    16'b1010111100001100  :  v_idivop[0] = 1'b1;
-	    // vdivu.vx - funct7=100000, funct3=100 (vm bit ignored)
-	    16'b1010111100000100  :  v_idivop[0] = 1'b1;
-	    // vrem.vx  - funct7=100011, funct3=100 (vm bit ignored)
-	    16'b1010111100011100  :  v_idivop[0] = 1'b1;
-	    // vremu.vx - funct7=100010, funct3=100 (vm bit ignored)
-	    16'b1010111100010100  :  v_idivop[0] = 1'b1;
+	    // vdiv.vv  - funct7[6:1]=100001, funct3=010 (vm bit ignored)
+	    16'b1010111_100001_010  :  v_idivop[0] = 1'b1;
+	    // vdivu.vv - funct7[6:1]=100000, funct3=000 (vm bit ignored)
+	    16'b1010111_100000_000  :  v_idivop[0] = 1'b1;
+	    // vrem.vv  - funct7[6:1]=100011, funct3=000 (vm bit ignored)
+	    16'b1010111_100011_000  :  v_idivop[0] = 1'b1;
+	    // vremu.vv - funct7[6:1]=100010, funct3=000 (vm bit ignored)
+	    16'b1010111_100010_000  :  v_idivop[0] = 1'b1;
+	    // vdiv.vx  - funct7[6:1]=100001, funct3=100 (vm bit ignored)
+	    16'b1010111_100001_100  :  v_idivop[0] = 1'b1;
+	    // vdivu.vx - funct7[6:1]=100000, funct3=100 (vm bit ignored)
+	    16'b1010111_100000_100  :  v_idivop[0] = 1'b1;
+	    // vrem.vx  - funct7[6:1]=100011, funct3=100 (vm bit ignored)
+	    16'b1010111_100011_100  :  v_idivop[0] = 1'b1;
+	    // vremu.vx - funct7[6:1]=100010, funct3=100 (vm bit ignored)
+	    16'b1010111_100010_100  :  v_idivop[0] = 1'b1;
 	    default : v_idivop[0] = 1'b0;
 	endcase
 end
@@ -8012,16 +8012,16 @@ output reg [0:0] v_fdivop
 
 always_comb begin
 	case({Opcode[6], Opcode[5], Opcode[4], Opcode[3], Opcode[2], Opcode[1], Opcode[0], funct7[6], funct7[5], funct7[4], funct7[3], funct7[2], funct7[1], funct3[2], funct3[1], funct3[0]})
-	    // vfdiv.vv  - funct7=100000, funct3=001 (vm bit ignored)
-	    16'b1010111100000001  :  v_fdivop[0] = 1'b1;
-	    // vfrdiv.vv - funct7=100001, funct3=001 (vm bit ignored)
-	    16'b1010111100001001  :  v_fdivop[0] = 1'b1;
+	    // vfdiv.vv  - funct7[6:1]=100000, funct3=001 (vm bit ignored)
+	    16'b1010111_100000_001  :  v_fdivop[0] = 1'b1;
+	    // vfrdiv.vv - funct7[6:1]=100001, funct3=001 (vm bit ignored)
+	    16'b1010111_100001_001  :  v_fdivop[0] = 1'b1;
 	    // vfsqrt.v, vfrsqrt7.v, vfrec7.v - funct7[6:1]=010011, funct3=001 (differentiated by vs1, vm bit ignored)
-	    16'b1010111010011001  :  v_fdivop[0] = 1'b1;
-	    // vfdiv.vf  - funct7=100000, funct3=101 (vm bit ignored)
-	    16'b1010111100000101  :  v_fdivop[0] = 1'b1;
-	    // vfrdiv.vf - funct7=100001, funct3=101 (vm bit ignored)
-	    16'b1010111100001101  :  v_fdivop[0] = 1'b1;
+	    16'b1010111_010011_001  :  v_fdivop[0] = 1'b1;
+	    // vfdiv.vf  - funct7[6:1]=100000, funct3=101 (vm bit ignored)
+	    16'b1010111_100000_101  :  v_fdivop[0] = 1'b1;
+	    // vfrdiv.vf - funct7[6:1]=100001, funct3=101 (vm bit ignored)
+	    16'b1010111_100001_101  :  v_fdivop[0] = 1'b1;
 	    default : v_fdivop[0] = 1'b0;
 	endcase
 end
